931a066525f4bfa73382eb7b18f843da5fe7a6bc
novoseltcev <novoseltcev.stanislav@gmail.com>
2022-09-14 18:36:29 +0300

    Add recreate db script

---
+++ app/scripts/recreate_db.py
@@ -0,0 +1,17 @@
+from app import create_app
+from app.db import db
+
+
+def main(dump_file='dumps/inserts.sql'):
+    create_app()
+    db.drop_all()
+    db.create_all()
+    try:
+        with open(dump_file) as f:
+            db.engine.execute(f.read())
+    except Exception:
+        pass
+
+
+if __name__ == '__main__':
+    main()
